7f31fd82e8cf64b4a306ca5013836613b495d16c
Alex Mireles <anchises1968@gmail.com>
2019-04-15 13:25:29 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Master and Commander</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
