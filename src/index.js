c87eb3a89d40a09c14293388e11c14678c028fcc
Alex Mireles <anchises1968@gmail.com>
2019-04-15 17:28:13 -0700

    added css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 const Index = () => {
-  return <div>Master and Commander</div>;
+  return (
+    <div>
+      <h1 style={{
+          fontSize:"3rem",
+          margin:"auto",
+          fontWeight:"300",
+          padding:"0 20px 0 20px",
+          textTransform:"uppercase",
+          textAlign:"center"
+        }}>MirelesCloud React-Webpack Basic Template</h1>
+      <p style={{
+          paddingTop:"50px",
+          fontSize:"1.2rem",
+          margin:"auto",
+          textAlign:"left"
+        }}>This is just a basic set up.  I have used this previously to set up my AWS portfolio and <a href="https://gemstateclassof68.com" target="_blank">Gem State Class of 1968</a>.
+        Got to make sure to keep babel packages congruent with each other.  Othwerwise it'll get a little squirly.
+      </p>
+    </div>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.js',
   module: {
     rules: [
       {
@@ -9,6 +10,24 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
